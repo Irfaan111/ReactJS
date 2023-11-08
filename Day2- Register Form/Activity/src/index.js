8fdcbb48758420d63504c369fb78c153270d7cfa
Irfaan111 <150134672+Irfaan111@users.noreply.github.com>
2023-11-08 10:26:05 +0530

    Add files via upload

---
+++ Day2- Register Form/Activity/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Style.css';
+import reportWebVitals from './reportWebVitals';
+import F3 from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <F3 />
+  </React.StrictMode>
+);
+reportWebVitals();
